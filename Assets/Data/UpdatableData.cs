eb54e12aa7a5c40272683df6f367296e1581929e
BocaDoru <bocaioandoru12@gmail.com>
2025-04-30 12:07:03 +0300

    first commit

---
+++ Assets/Data/UpdatableData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatableData : ScriptableObject
+{
+    public event System.Action OnValuesUpdated;
+    public bool autoUpdate;
+    protected virtual void OnValidate()
+    {
+        if (autoUpdate)
+            NotifyUpdateValues();
+    }
+    public void NotifyUpdateValues()
+    {
+        if (OnValuesUpdated != null)
+            OnValuesUpdated();
+    }
+}
